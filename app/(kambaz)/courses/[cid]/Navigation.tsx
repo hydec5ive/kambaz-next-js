ea923310b5fbbee919ed6aecc24eda88995e0ee8
gojo1207 <159972308+hydec5ive@users.noreply.github.com>
2026-02-19 09:35:15 -0500

    a3 JavaScript

--- app/(kambaz)/courses/[cid]/Navigation.tsx
+++ app/(kambaz)/courses/[cid]/Navigation.tsx
@@ -5,21 +5,32 @@ import { useParams, usePathname } from "next/navigation";
 export default function CourseNavigation() {
   const { cid } = useParams();
   const pathname = usePathname();
-  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+
+  const links = [
+    { label: "Home", path: "home" },
+    { label: "Modules", path: "modules" },
+    { label: "Piazza", path: "piazza" },
+    { label: "Zoom", path: "zoom" },
+    { label: "Assignments", path: "assignments" },
+    { label: "Quizzes", path: "quizzes" },
+    { label: "Grades", path: "grades" },
+    { label: "People", path: "people/table" },
+  ];
 
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
         <Link
-          key={link}
-          href={`/courses/${cid}/${link === "People" ? "people/table" : link.toLowerCase()}`}
-          id={`wd-course-${link.toLowerCase()}-link`}
+          key={link.label}
+          href={`/courses/${cid}/${link.path}`}
+          id={`wd-course-${link.label.toLowerCase()}-link`}
           className={`list-group-item border-0 ${
-            pathname.includes(link.toLowerCase()) ? "active" : "text-danger"
+            pathname.includes(link.label.toLowerCase()) ? "active" : "text-danger"
           }`}
         >
-          {link}
+          {link.label}
         </Link>
       ))}
     </div>
-  );}
+  );
+}
